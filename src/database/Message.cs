391de0c7f3e869e0c4cd8ba8d6c30bb095af2ba8
totodore <prevottheodore@gmail.com>
2021-05-12 05:00:29 +0100

    patching enum issues

--- src/database/Message.cs
+++ src/database/Message.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.Serialization;
 
 namespace DatabaseHandler
 {
@@ -16,8 +18,20 @@ namespace DatabaseHandler
 		public string ParsedMessage { get; set; }
 		public string Message { get; set; }
 		public string Description { get; set; }
-		public MessageType Type { get; set; }
+		[NotMapped]
+		public MessageType Type
+		{
+			get
+			{
+				return Enum.GetValues<MessageType>()[TypeEnum];
+			}
+			set
+			{
+				TypeEnum = Enum.GetValues<MessageType>().ToList().IndexOf(value);
+			}
+		}
 
+		public int TypeEnum { get; set; }
 		public string GuildId { get; set; }
 		public GuildEntity Guild { get; set; }
 		public string CreatorId { get; set; }
