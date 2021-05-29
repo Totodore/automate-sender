db1980a95db678e0acacbf16adfbebd5dce220eb
Totodore <prevottheodore@gmail.com>
2021-05-29 02:53:44 +0100

    getter to get the first day of the current month (DateTime)

--- src/utils/TimeHelpers.cs
+++ src/utils/TimeHelpers.cs
@@ -6,7 +6,7 @@ namespace AutomateSender
 	{
 		public static long CurrentTimeMillis()
 		{
-			return DateTimeOffset.Now.ToUnixTimeMilliseconds();
+			return DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
 		}
 		/// <summary>
 		/// Convert a timestamp to a UTC DateTime
@@ -19,5 +19,9 @@ namespace AutomateSender
 			DateTime dtDateTime = new(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
 			return dtDateTime.AddMilliseconds(unixTimeStamp);
 		}
+
+		/// <summary>Get the current month</summary>
+		/// <returns>The first day of the month in DateTime UTC</returns>
+		public static DateTime CurrentMonth { get { return new DateTime(DateTime.UtcNow.Year, DateTime.UtcNow.Month, 1); } }
 	}
 }
