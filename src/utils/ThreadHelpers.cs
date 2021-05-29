0d9a14886918ca81f06b90d04f21b677ceb5be76
Totodore <prevottheodore@gmail.com>
2021-05-29 02:53:13 +0100

    adding thread wait handler

--- src/utils/ThreadHelpers.cs
+++ src/utils/ThreadHelpers.cs
@@ -1,29 +1,40 @@
 using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AutomateSender.DatabaseHandler;
 
 namespace AutomateSender
 {
 	public static class ThreadHelpers
 	{
 		/// <summary>
-		/// Wait for threads in threadpool
+		/// Create a threadpool from a list of functions and with an optional timeout
 		/// </summary>
-		/// <param name="timeOutSeconds">The timeout in seconds for the waiting</param>
-		/// <returns>True if it timed out, false if everything went ok</returns>
-		public static bool WaitForThreads(int timeOutSeconds)
+		/// <param name="actions">The list of the function that returns a T arg</param>
+		/// <param name="timeout">The timeout of the threadpool</param>
+		/// <typeparam name="T">The return type of the lambda list</typeparam>
+		/// <returns>The list of the lambda response</returns>
+		public static List<T> SpawnAndWait<T>(List<Func<Task<T>>> actions, int timeout = -1)
 		{
-			//Now wait until all threads from the Threadpool have returned
-			while (timeOutSeconds > 0)
-			{
-				//figure out what the max worker thread count it
-				System.Threading.ThreadPool.GetMaxThreads(out int maxThreads, out _);
-				System.Threading.ThreadPool.GetAvailableThreads(out int availThreads, out _);
-
-				if (availThreads == maxThreads) return false;
-				// Sleep
-				System.Threading.Thread.Sleep(TimeSpan.FromMilliseconds(1000));
-				--timeOutSeconds;
-			}
-			return true;
+				var handles = new EventWaitHandle[actions.Count];
+				var els = new List<T>();
+				for (var i = 0; i < actions.Count; i++)
+				{
+					handles[i] = new ManualResetEvent(false);
+					var currentAction = actions[i];
+					var currentHandle = handles[i];
+					ThreadPool.QueueUserWorkItem(async _ => {
+						try {
+							els.Add(await currentAction());
+						} finally {
+							currentHandle.Set();
+						}
+					});
+				}
+				if (handles.Length > 0)
+					WaitHandle.WaitAll(handles, timeout);
+				return els;
 		}
 	}
 }
