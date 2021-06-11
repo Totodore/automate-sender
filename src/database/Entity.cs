1b09e00122b0dc99eac537fa12fee9ac90150539
Totodore <prevottheodore@gmail.com>
2021-06-11 12:42:12 +0200

    patching entity system with to string

--- src/database/Entity.cs
+++ src/database/Entity.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Serilog;
+
 namespace AutomateSender.DatabaseHandler
 {
 	public abstract class Entity
@@ -5,8 +11,12 @@ namespace AutomateSender.DatabaseHandler
 		public override string ToString()
 		{
 			string result = GetType().Name + " {\n";
+			List<string> objects = GetType().GetProperties().Where(el => el.Name.EndsWith("Id")).ToList().ConvertAll(el => Regex.Replace(el.Name, "[a-z][A-Z]", m => m.Value[0] + " " + char.ToLower(m.Value[1])).Split(" ")[0]);
 			foreach (var prop in GetType().GetProperties())
-				result += "\t" + prop.Name + ": " + prop.GetValue(this, null)?.ToString() + "\n";
+			{
+				if (!objects.Contains(prop.Name))	//This condition is to avoid stack overflow with nested Entity inherited properties
+					result += "\t" + prop.Name + ": " + prop.GetValue(this, null)?.ToString() + "\n";
+			}
 			result += "}";
 			return result;
 		}
